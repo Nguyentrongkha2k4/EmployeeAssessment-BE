6739baf4734f7fb0a21bf70be0e50edb9a33d41e
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2025-03-21 14:53:30 +0700

    BE init

---
+++ src/main/java/com/phucislearning/employeeassessmentbe/EmployeeAssessmentBeApplication.java
@@ -0,0 +1,13 @@
+package com.phucislearning.employeeassessmentbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAssessmentBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAssessmentBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phucislearning/employeeassessmentbe/EmployeeAssessmentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucislearning.employeeassessmentbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAssessmentBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
