137b85611352d91a8cff13257641a69d9758b21e
Nguyen Trong Kha <trongkha08022k4@gmail.com>
2025-03-22 10:34:48 +0700

    config security

---
+++ src/main/java/com/brainnotfound/employeeassessmentbe/configuration/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.brainnotfound.employeeassessmentbe.configuration;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    private final String[] PUBLIC_ENDPOINTS = {
+        "/"
+    };
+
+    @Value("${jwt.secret_key}")
+    private String SECRECT_KEY;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.cors(cors -> cors.configurationSource(request -> {
+                        var corsConfig = new CorsConfiguration();
+                        corsConfig.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:3000"));
+                        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                        corsConfig.setAllowedHeaders(List.of("*"));
+                        corsConfig.setExposedHeaders(List.of("Authorization", "Content-Type"));
+                        corsConfig.setAllowCredentials(true);
+                        return corsConfig;
+                    }))
+                    .authorizeHttpRequests(request -> 
+                        request
+                        .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                        .anyRequest().authenticated())
+                    .csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+}
