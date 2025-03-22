b93b7ac9e1cf082252d5bbb168c648f50c4060d7
blueonline07 <ldkhang1201@gmail.com>
2025-03-22 10:10:58 +0700

    add user model, not tested

---
+++ src/main/java/com/brainnotfound/employeeassessmentbe/services/UserService.java
@@ -0,0 +1,19 @@
+package com.brainnotfound.employeeassessmentbe.services;
+
+import com.brainnotfound.employeeassessmentbe.models.User;
+import com.brainnotfound.employeeassessmentbe.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
