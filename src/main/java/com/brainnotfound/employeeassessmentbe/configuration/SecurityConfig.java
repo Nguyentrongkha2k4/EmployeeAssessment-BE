bf514c9f9323ab4b22cd1e45afa19aa24cd8df5d
tison2810 <son.nguyenthai@hcmut.edu.vn>
2025-03-22 11:43:37 +0700

    login + register api

--- src/main/java/com/brainnotfound/employeeassessmentbe/configuration/SecurityConfig.java
+++ src/main/java/com/brainnotfound/employeeassessmentbe/configuration/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -16,7 +18,9 @@ import org.springframework.web.cors.CorsConfiguration;
 @EnableMethodSecurity
 public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINTS = {
-        "/"
+        "/",
+        "api/auth/login",
+        "/api/auth/register"
     };
 
     @Bean
@@ -36,5 +40,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                     .csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/brainnotfound/employeeassessmentbe/utils/Utils.java
@@ -0,0 +1,7 @@
+// package com.brainnotfound.employeeassessmentbe.utils;
+
+// public class Utils {
+//     public static final String HashPassword(String password) {
+//         return password;
+//     }
+// }
