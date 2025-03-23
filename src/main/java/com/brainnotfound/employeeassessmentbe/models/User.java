480e90412cbee25a32f4087406a67c4a14419285
Nguyen Trong Kha <trongkha08022k4@gmail.com>
2025-03-23 12:36:53 +0700

    refactor structure

--- src/main/java/com/brainnotfound/employeeassessmentbe/models/User.java
+++ src/main/java/com/brainnotfound/employeeassessmentbe/models/User.java
@@ -1,19 +1,28 @@
 package com.brainnotfound.employeeassessmentbe.models;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,8 +34,11 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
+    private String role;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonIgnore
-    private List<Assessment> assessments = new ArrayList<>();
+    private List<Assessment> assessments;
 
 }
