81c520087ec2b1aba098c025cfed09f326b5f27c
blueonline07 <ldkhang1201@gmail.com>
2025-03-27 02:14:53 +0700

    add FeedbackController.java

---
+++ src/main/java/com/brainnotfound/employeeassessmentbe/controllers/FeedbackController.java
@@ -0,0 +1,10 @@
+package com.brainnotfound.employeeassessmentbe.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/assessment/my/feedback")
+public class FeedbackController {
+
+}
