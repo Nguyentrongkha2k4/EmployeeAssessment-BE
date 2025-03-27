6c0fcaa645555bc484ea911e2bd92fd250935276
blueonline07 <ldkhang1201@gmail.com>
2025-03-28 00:50:08 +0700

    fix CriteriaService

--- src/main/java/com/brainnotfound/employeeassessmentbe/services/CriteriaService.java
+++ src/main/java/com/brainnotfound/employeeassessmentbe/services/CriteriaService.java
@@ -4,12 +4,15 @@ import com.brainnotfound.employeeassessmentbe.models.Criteria;
 import com.brainnotfound.employeeassessmentbe.repositories.AssessmentRepository;
 import com.brainnotfound.employeeassessmentbe.repositories.CriteriaRepository;
 import com.brainnotfound.employeeassessmentbe.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class CriteriaService {
+    @Autowired
     private CriteriaRepository criteriaRepository;
+
     public Criteria save (Criteria criteria) {
         return criteriaRepository.save(criteria);
     }
