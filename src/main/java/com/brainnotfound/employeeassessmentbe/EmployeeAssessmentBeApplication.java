5bc1f6316278902116e4746b2069298d8b75ceda
tison2810 <son.nguyenthai@hcmut.edu.vn>
2025-03-27 21:50:04 +0700

    deploy env

--- src/main/java/com/brainnotfound/employeeassessmentbe/EmployeeAssessmentBeApplication.java
+++ src/main/java/com/brainnotfound/employeeassessmentbe/EmployeeAssessmentBeApplication.java
@@ -3,10 +3,20 @@ package com.brainnotfound.employeeassessmentbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class EmployeeAssessmentBeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EmployeeAssessmentBeApplication.class, args);
-    }
+    // public static void main(String[] args) {
+    //     SpringApplication.run(EmployeeAssessmentBeApplication.class, args);
+    // }
+    	static {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAssessmentBeApplication.class, args);
+	}
 }
