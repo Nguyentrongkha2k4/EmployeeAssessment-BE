eb9ed9ecb7bd122a5c01ee6892a32e91256b0d1a
tison2810 <son.nguyenthai@hcmut.edu.vn>
2025-03-28 22:15:57 +0700

    req body from resp to req assessment

--- src/main/java/com/brainnotfound/employeeassessmentbe/DTO/request/AssessmentRequest.java
+++ src/main/java/com/brainnotfound/employeeassessmentbe/DTO/request/AssessmentRequest.java
@@ -9,4 +9,11 @@ public class AssessmentRequest {
     private Integer score;
     private Long userId;
     private String comment;
+
+    public AssessmentRequest(Long userId, Long criteriaId, Integer score, String comment) {
+        this.userId = userId;
+        this.criteriaId = criteriaId;
+        this.score = score;
+        this.comment = comment;
+    }
 }
